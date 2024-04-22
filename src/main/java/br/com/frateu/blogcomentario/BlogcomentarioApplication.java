a8904ab59b6ff776eca771e667df6bd1e3f2f126
Matheus Freitas <matheus.fanjos@outlook.com>
2024-04-21 23:22:29 -0300

    Esqueleto do projeto.

---
+++ src/main/java/br/com/frateu/blogcomentario/BlogcomentarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.frateu.blogcomentario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcomentarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogcomentarioApplication.class, args);
+	}
+
+}
